4a9f9d10155700d9bce0a6cfad8c952a3324b772
Igor Fedotov <igor.fedotov@actonica.ru>
2020-09-21 00:02:53 +0700

    add uniques sql script

---
+++ sqls/3.uniques.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE FUNCTION where_unique(val IN VARCHAR2)
+    RETURN INTEGER IS
+    chars_unique VARCHAR2(2000);
+BEGIN
+    chars_unique := '';
+    for i in 1..LENGTH(val)
+        loop
+            IF REGEXP_COUNT(val, SUBSTR(val, i, 1)) > 1 THEN
+                RETURN 0;
+            end if;
+        end loop;
+    RETURN 1;
+EXCEPTION
+    WHEN VALUE_ERROR THEN RETURN 0;
+END where_unique;
+
+select FID from SYS.T1 where REGEXP_LIKE(FID, '^[0-9]+$') and where_unique(FID) = 1;
+
